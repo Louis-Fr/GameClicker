ba2d357753217c708464cdee651d5efcc7b2a702
Louis Rey <louisreypro@yahoo.com>
2022-10-25 13:02:44 +0200

    Initial Commit

---
+++ Clicker/ClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickerApp.swift
+//  Clicker
+//
+//  Created by Louis REY on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
